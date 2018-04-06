5f90d1e1f071dd66d79dcc56888f8bfc942507ac
Jim McGee <jmcgee86@gmail.com>
2018-04-06 18:01:46 +0000

    add comments for initials-more-comfortable

--- intials_more/initials.c
+++ intials_more/initials.c
@@ -13,16 +13,16 @@ int main (void)
     }
     while (!name);
 
-    for (int i=0; i<strlen(name); i++)
+    for (int i=0; i<strlen(name); i++)//iterates through each char in name string
     {
-        if(!isalpha(name[i]))
+        if(!isalpha(name[i]))//checks if the char is not alphabetic, ie a space
         {
-            ready = true;
+            ready = true; // if char is not alphabetic, set ready to true
         }
-        if(isalpha(name[i]) && ready)
+        if(isalpha(name[i]) && ready) // if char is alpha and ready is true...
         {
-            printf("%c",toupper(name[i]));
-            ready = !ready;
+            printf("%c",toupper(name[i])); //prints letter char in upper case
+            ready = !ready; //and sets ready to false
         }
     }
         printf("\n");
